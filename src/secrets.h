141f12ce0d26bc1c0136ef65e7f6d678a430de4f
pnot0 <39804244+pnot0@users.noreply.github.com>
2025-04-29 17:53:05 -0300

    update

--- src/secrets.h
+++
@@ -1,8 +0,0 @@
-/*
-This will include all information that i dont want to be public,
-like network SSID and password, spotify API key and so on
-*/
-
-const char* ssid = "CasaNucci";
-const char* password = "CASANUCCI171";
-String clientID = "f8c8cb559b5245328a674b2e1771478e";
